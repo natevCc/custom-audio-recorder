e590da9a275d1cd706f872a080614c8eef0247d5
Tsvetan <vasilevcvtn@gmail.com>
2023-11-23 01:57:37 +0200

    test 3

--- js/index.js
+++ js/index.js
@@ -14,77 +14,102 @@ recordBtn.addEventListener("click", () => {
   setTimeout(stopAudioRecording, 5000);
 });
 
-async function startAudioRecording() {
+function startAudioRecording() {
   console.log("Recording Audio...");
   setButtonText("Recording...");
 
-  try {
-    await audioRecorder.start();
-  } catch (error) {
-    //on error
-    //No Browser Support Error
-    if (
-      error.message.includes(
-        "mediaDevices API or getUserMedia method is not supported in this browser."
-      )
-    ) {
-      console.log("To record audio, use browsers like Chrome and Firefox.");
-      displayBrowserNotSupportedOverlay();
-    }
+  //start recording using the audio recording API
+  audioRecorder
+    .start()
+    .then(() => {
+      //on success
+      //store the recording start time to display the elapsed time according to it
+      //  audioRecordStartTime = new Date();
+    })
+    .catch((error) => {
+      //on error
+      //No Browser Support Error
+      if (
+        error.message.includes(
+          "mediaDevices API or getUserMedia method is not supported in this browser."
+        )
+      ) {
+        console.log("To record audio, use browsers like Chrome and Firefox.");
+        displayBrowserNotSupportedOverlay();
+      }
 
-    //Error handling structure
-    switch (error.name) {
-      case "AbortError": //error from navigator.mediaDevices.getUserMedia
-        console.log("An AbortError has occured.");
-        break;
-      case "NotAllowedError": //error from navigator.mediaDevices.getUserMedia
-        console.log(
-          "A NotAllowedError has occured. User might have denied permission."
-        );
-        break;
-      case "NotFoundError": //error from navigator.mediaDevices.getUserMedia
-        console.log("A NotFoundError has occured.");
-        break;
-      case "NotReadableError": //error from navigator.mediaDevices.getUserMedia
-        console.log("A NotReadableError has occured.");
-        break;
-      case "SecurityError": //error from navigator.mediaDevices.getUserMedia or from the MediaRecorder.start
-        console.log("A SecurityError has occured.");
-        break;
-      case "TypeError": //error from navigator.mediaDevices.getUserMedia
-        console.log("A TypeError has occured.");
-        break;
-      case "InvalidStateError": //error from the MediaRecorder.start
-        console.log("An InvalidStateError has occured.");
-        break;
-      case "UnknownError": //error from the MediaRecorder.start
-        console.log("An UnknownError has occured.");
-        break;
-      default:
-        console.log("An error occured with the error name " + error.name);
-    }
-  }
+      //Error handling structure
+      switch (error.name) {
+        case "AbortError": //error from navigator.mediaDevices.getUserMedia
+          console.log("An AbortError has occured.");
+          break;
+        case "NotAllowedError": //error from navigator.mediaDevices.getUserMedia
+          console.log(
+            "A NotAllowedError has occured. User might have denied permission."
+          );
+          break;
+        case "NotFoundError": //error from navigator.mediaDevices.getUserMedia
+          console.log("A NotFoundError has occured.");
+          break;
+        case "NotReadableError": //error from navigator.mediaDevices.getUserMedia
+          console.log("A NotReadableError has occured.");
+          break;
+        case "SecurityError": //error from navigator.mediaDevices.getUserMedia or from the MediaRecorder.start
+          console.log("A SecurityError has occured.");
+          break;
+        case "TypeError": //error from navigator.mediaDevices.getUserMedia
+          console.log("A TypeError has occured.");
+          break;
+        case "InvalidStateError": //error from the MediaRecorder.start
+          console.log("An InvalidStateError has occured.");
+          break;
+        case "UnknownError": //error from the MediaRecorder.start
+          console.log("An UnknownError has occured.");
+          break;
+        default:
+          console.log("An error occured with the error name " + error.name);
+      }
+    });
 }
 
-async function stopAudioRecording() {
+function stopAudioRecording() {
   console.log("Stopping Audio Recording...");
 
-  try {
-    const audioBlob = await audioRecorder.stop();
-    playAudio(audioBlob, true);
-  } catch (error) {
-    //Error handling structure
-    switch (error.name) {
-      case "InvalidStateError": //error from the MediaRecorder.stop
-        console.log("An InvalidStateError has occured.");
-        break;
-      default:
-        console.log("An error occured with the error name " + error.name);
-    }
-  }
+  //stop the recording using the audio recording API
+  audioRecorder
+    .stop()
+    .then((audioAsblob) => {
+      //Play recorder audio
+      playAudio(audioAsblob, true);
+    })
+    .catch((error) => {
+      //Error handling structure
+      switch (error.name) {
+        case "InvalidStateError": //error from the MediaRecorder.stop
+          console.log("An InvalidStateError has occured.");
+          break;
+        default:
+          console.log("An error occured with the error name " + error.name);
+      }
+    });
+
+  // try {
+  //   const audioBlob = await audioRecorder.stop();
+  //   playAudio(audioBlob, true);
+  // } catch (error) {
+  //   //Error handling structure
+  //   switch (error.name) {
+  //     case "InvalidStateError": //error from the MediaRecorder.stop
+  //       console.log("An InvalidStateError has occured.");
+  //       break;
+  //     default:
+  //       console.log("An error occured with the error name " + error.name);
+  //   }
+  // }
 }
 
 function playAudio(audioBlob, useSource) {
+  setButtonText("Playing...");
   if (useSource) {
     //read content of files (Blobs) asynchronously
     let reader = new FileReader();
@@ -125,7 +150,6 @@ function playAudio(audioBlob, useSource) {
     //read content and convert it to a URL (base64)
     reader.readAsDataURL(audioBlob);
   } else {
-    setButtonText("Playing...");
     const audioUrl = URL.createObjectURL(audioBlob);
     const audio = new Audio(audioUrl);
     audio.play();
