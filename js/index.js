bf4a378cd6f2ddaac86287804851c22874ef463b
tsvetan-vr <tsvetan.vasilev@metasim.io>
2023-11-23 18:01:20 +0200

    cloned audio-recorder-demo

--- js/index.js
+++ js/index.js
@@ -1,37 +1,137 @@
-// import AudioRecorder from "./audiorecorder.js";
+// index.js ---------------
+//Model
+//none
 
-const recordBtn = document.getElementById("recordBtn");
-const audioElement = document.getElementById("audioElement");
-let audioElementSource;
-let audioRecordStartTime;
+//View
+var microphoneButton = document.getElementsByClassName(
+  "start-recording-button"
+)[0];
+var recordingControlButtonsContainer = document.getElementsByClassName(
+  "recording-contorl-buttons-container"
+)[0];
+var stopRecordingButton = document.getElementsByClassName(
+  "stop-recording-button"
+)[0];
+var cancelRecordingButton = document.getElementsByClassName(
+  "cancel-recording-button"
+)[0];
+var elapsedTimeTag = document.getElementsByClassName("elapsed-time")[0];
+var closeBrowserNotSupportedBoxButton = document.getElementsByClassName(
+  "close-browser-not-supported-box"
+)[0];
+var overlay = document.getElementsByClassName("overlay")[0];
+var audioElement = document.getElementsByClassName("audio-element")[0];
+var audioElementSource = document
+  .getElementsByClassName("audio-element")[0]
+  .getElementsByTagName("source")[0];
+var textIndicatorOfAudiPlaying = document.getElementsByClassName(
+  "text-indication-of-audio-playing"
+)[0];
 
-setButtonText("Record");
-audioElement.onended = handleAudioEnded;
+//Listeners
 
-function handleAudioEnded() {
-  console.log("Audio ended");
-  recordBtn.disabled = false;
-  setButtonText("Record");
+//Listen to start recording button
+microphoneButton.onclick = startAudioRecording;
+
+//Listen to stop recording button
+stopRecordingButton.onclick = stopAudioRecording;
+
+//Listen to cancel recording button
+cancelRecordingButton.onclick = cancelAudioRecording;
+
+//Listen to when the ok button is clicked in the browser not supporting audio recording box
+closeBrowserNotSupportedBoxButton.onclick = hideBrowserNotSupportedOverlay;
+
+//Listen to when the audio being played ends
+audioElement.onended = hideTextIndicatorOfAudioPlaying;
+
+/** Displays recording control buttons */
+function handleDisplayingRecordingControlButtons() {
+  //Hide the microphone button that starts audio recording
+  microphoneButton.style.display = "none";
+
+  //Display the recording control buttons
+  recordingControlButtonsContainer.classList.remove("hide");
+
+  //Handle the displaying of the elapsed recording time
+  handleElapsedRecordingTime();
 }
 
-recordBtn.addEventListener("click", () => {
-  recordBtn.disabled = true;
-  startAudioRecording();
+/** Hide the displayed recording control buttons */
+function handleHidingRecordingControlButtons() {
+  //Display the microphone button that starts audio recording
+  microphoneButton.style.display = "block";
 
-  setTimeout(stopAudioRecording, 5000);
-});
+  //Hide the recording control buttons
+  recordingControlButtonsContainer.classList.add("hide");
 
+  //stop interval that handles both time elapsed and the red dot
+  clearInterval(elapsedTimeTimer);
+}
+
+/** Displays browser not supported info box for the user*/
+function displayBrowserNotSupportedOverlay() {
+  overlay.classList.remove("hide");
+}
+
+/** Displays browser not supported info box for the user*/
+function hideBrowserNotSupportedOverlay() {
+  overlay.classList.add("hide");
+}
+
+/** Creates a source element for the the audio element in the HTML document*/
+function createSourceForAudioElement() {
+  let sourceElement = document.createElement("source");
+  audioElement.appendChild(sourceElement);
+
+  audioElementSource = sourceElement;
+}
+
+/** Display the text indicator of the audio being playing in the background */
+function displayTextIndicatorOfAudioPlaying() {
+  textIndicatorOfAudiPlaying.classList.remove("hide");
+}
+
+/** Hide the text indicator of the audio being playing in the background */
+function hideTextIndicatorOfAudioPlaying() {
+  textIndicatorOfAudiPlaying.classList.add("hide");
+}
+
+//Controller
+
+/** Stores the actual start time when an audio recording begins to take place to ensure elapsed time start time is accurate*/
+var audioRecordStartTime;
+
+/** Stores the maximum recording time in hours to stop recording once maximum recording hour has been reached */
+var maximumRecordingTimeInHours = 1;
+
+/** Stores the reference of the setInterval function that controls the timer in audio recording*/
+var elapsedTimeTimer;
+
+/** Starts the audio recording*/
 function startAudioRecording() {
   console.log("Recording Audio...");
-  setButtonText("Recording...");
+
+  //If a previous audio recording is playing, pause it
+  let recorderAudioIsPlaying = !audioElement.paused; // the paused property tells whether the media element is paused or not
+  console.log("paused?", !recorderAudioIsPlaying);
+  if (recorderAudioIsPlaying) {
+    audioElement.pause();
+    //also hide the audio playing indicator displayed on the screen
+    hideTextIndicatorOfAudioPlaying();
+  }
 
   //start recording using the audio recording API
   audioRecorder
     .start()
     .then(() => {
       //on success
+
       //store the recording start time to display the elapsed time according to it
       audioRecordStartTime = new Date();
+
+      //display control buttons to offer the functionality of stop and cancel
+      handleDisplayingRecordingControlButtons();
     })
     .catch((error) => {
       //on error
@@ -78,7 +178,8 @@ function startAudioRecording() {
       }
     });
 }
-
+/** Stop the currently started audio recording & sends it
+ */
 function stopAudioRecording() {
   console.log("Stopping Audio Recording...");
 
@@ -87,7 +188,10 @@ function stopAudioRecording() {
     .stop()
     .then((audioAsblob) => {
       //Play recorder audio
-      playAudio(audioAsblob, true);
+      playAudio(audioAsblob);
+
+      //hide recording control button & return record icon
+      handleHidingRecordingControlButtons();
     })
     .catch((error) => {
       //Error handling structure
@@ -99,84 +203,160 @@ function stopAudioRecording() {
           console.log("An error occured with the error name " + error.name);
       }
     });
-
-  // try {
-  //   const audioBlob = await audioRecorder.stop();
-  //   playAudio(audioBlob, true);
-  // } catch (error) {
-  //   //Error handling structure
-  //   switch (error.name) {
-  //     case "InvalidStateError": //error from the MediaRecorder.stop
-  //       console.log("An InvalidStateError has occured.");
-  //       break;
-  //     default:
-  //       console.log("An error occured with the error name " + error.name);
-  //   }
-  // }
 }
 
-function playAudio(audioBlob, useSource) {
-  setButtonText("Playing...");
-  if (useSource) {
-    //read content of files (Blobs) asynchronously
-    let reader = new FileReader();
+/** Cancel the currently started audio recording */
+function cancelAudioRecording() {
+  console.log("Canceling audio...");
 
-    //once content has been read
-    reader.onload = (e) => {
-      //store the base64 URL that represents the URL of the recording audio
-      let base64URL = e.target.result;
+  //cancel the recording using the audio recording API
+  audioRecorder.cancel();
 
-      //If this is the first audio playing, create a source element
-      //as pre populating the HTML with a source of empty src causes error
-      if (!audioElementSource)
-        //if its not defined create it (happens first time only)
-        createSourceForAudioElement();
+  //hide recording control button & return record icon
+  handleHidingRecordingControlButtons();
+}
 
-      //set the audio element's source using the base64 URL
-      audioElementSource.src = base64URL;
+/** Plays recorded audio using the audio element in the HTML document
+ * @param {Blob} recorderAudioAsBlob - recorded audio as a Blob Object
+ */
+function playAudio(recorderAudioAsBlob) {
+  //read content of files (Blobs) asynchronously
+  let reader = new FileReader();
 
-      //set the type of the audio element based on the recorded audio's Blob type
-      let BlobType = audioBlob.type.includes(";")
-        ? audioBlob.type.substring(0, audioBlob.type.indexOf(";"))
-        : audioBlob.type;
-      audioElementSource.type = BlobType;
+  //once content has been read
+  reader.onload = (e) => {
+    //store the base64 URL that represents the URL of the recording audio
+    let base64URL = e.target.result;
 
-      //call the load method as it is used to update the audio element after changing the source or other settings
-      audioElement.load();
+    //If this is the first audio playing, create a source element
+    //as pre populating the HTML with a source of empty src causes error
+    if (!audioElementSource)
+      //if its not defined create it (happens first time only)
+      createSourceForAudioElement();
 
-      //play the audio after successfully setting new src and type that corresponds to the recorded audio
-      console.log("Playing audio...");
-      audioElement.play();
+    //set the audio element's source using the base64 URL
+    audioElementSource.src = base64URL;
 
-      // audioElement.addEventListener("ended", () => {
-      //   console.log("Audio ended");
-      //   recordBtn.disabled = false;
-      //   setButtonText("Record");
-      // });
-    };
-    //read content and convert it to a URL (base64)
-    reader.readAsDataURL(audioBlob);
-  } else {
-    const audioUrl = URL.createObjectURL(audioBlob);
-    const audio = new Audio(audioUrl);
-    audio.play();
+    //set the type of the audio element based on the recorded audio's Blob type
+    let BlobType = recorderAudioAsBlob.type.includes(";")
+      ? recorderAudioAsBlob.type.substr(
+          0,
+          recorderAudioAsBlob.type.indexOf(";")
+        )
+      : recorderAudioAsBlob.type;
+    audioElementSource.type = BlobType;
 
-    audio.addEventListener("ended", () => {
-      console.log("Audio ended");
-      recordBtn.disabled = false;
-      setButtonText("Record");
-    });
+    //call the load method as it is used to update the audio element after changing the source or other settings
+    audioElement.load();
+
+    //play the audio after successfully setting new src and type that corresponds to the recorded audio
+    console.log("Playing audio...");
+    audioElement.play();
+
+    //Display text indicator of having the audio play in the background
+    displayTextIndicatorOfAudioPlaying();
+  };
+
+  //read content and convert it to a URL (base64)
+  reader.readAsDataURL(recorderAudioAsBlob);
+}
+
+/** Computes the elapsed recording time since the moment the function is called in the format h:m:s*/
+function handleElapsedRecordingTime() {
+  //display inital time when recording begins
+  displayElapsedTimeDuringAudioRecording("00:00");
+
+  //create an interval that compute & displays elapsed time, as well as, animate red dot - every second
+  elapsedTimeTimer = setInterval(() => {
+    //compute the elapsed time every second
+    let elapsedTime = computeElapsedTime(audioRecordStartTime); //pass the actual record start time
+    //display the elapsed time
+    displayElapsedTimeDuringAudioRecording(elapsedTime);
+  }, 1000); //every second
+}
+
+/** Display elapsed time during audio recording
+ * @param {String} elapsedTime - elapsed time in the format mm:ss or hh:mm:ss
+ */
+function displayElapsedTimeDuringAudioRecording(elapsedTime) {
+  //1. display the passed elapsed time as the elapsed time in the elapsedTime HTML element
+  elapsedTimeTag.innerHTML = elapsedTime;
+
+  //2. Stop the recording when the max number of hours is reached
+  if (elapsedTimeReachedMaximumNumberOfHours(elapsedTime)) {
+    stopAudioRecording();
   }
 }
 
-/** Creates a source element for the the audio element in the HTML document*/
-function createSourceForAudioElement() {
-  let sourceElement = document.createElement("source");
-  audioElement.appendChild(sourceElement);
+/**
+ * @param {String} elapsedTime - elapsed time in the format mm:ss or hh:mm:ss
+ * @returns {Boolean} whether the elapsed time reached the maximum number of hours or not
+ */
+function elapsedTimeReachedMaximumNumberOfHours(elapsedTime) {
+  //Split the elapsed time by the symbo :
+  let elapsedTimeSplitted = elapsedTime.split(":");
 
-  audioElementSource = sourceElement;
+  //Turn the maximum recording time in hours to a string and pad it with zero if less than 10
+  let maximumRecordingTimeInHoursAsString =
+    maximumRecordingTimeInHours < 10
+      ? "0" + maximumRecordingTimeInHours
+      : maximumRecordingTimeInHours.toString();
+
+  //if it the elapsed time reach hours and also reach the maximum recording time in hours return true
+  if (
+    elapsedTimeSplitted.length === 3 &&
+    elapsedTimeSplitted[0] === maximumRecordingTimeInHoursAsString
+  )
+    return true;
+  //otherwise, return false
+  else return false;
 }
 
-function setButtonText(text) {
-  recordBtn.innerHTML = text;
+/** Computes the elapsedTime since the moment the function is called in the format mm:ss or hh:mm:ss
+ * @param {String} startTime - start time to compute the elapsed time since
+ * @returns {String} elapsed time in mm:ss format or hh:mm:ss format, if elapsed hours are 0.
+ */
+function computeElapsedTime(startTime) {
+  //record end time
+  let endTime = new Date();
+
+  //time difference in ms
+  let timeDiff = endTime - startTime;
+
+  //convert time difference from ms to seconds
+  timeDiff = timeDiff / 1000;
+
+  //extract integer seconds that dont form a minute using %
+  let seconds = Math.floor(timeDiff % 60); //ignoring uncomplete seconds (floor)
+
+  //pad seconds with a zero if neccessary
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  //convert time difference from seconds to minutes using %
+  timeDiff = Math.floor(timeDiff / 60);
+
+  //extract integer minutes that don't form an hour using %
+  let minutes = timeDiff % 60; //no need to floor possible incomplete minutes, becase they've been handled as seconds
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  //convert time difference from minutes to hours
+  timeDiff = Math.floor(timeDiff / 60);
+
+  //extract integer hours that don't form a day using %
+  let hours = timeDiff % 24; //no need to floor possible incomplete hours, becase they've been handled as seconds
+
+  //convert time difference from hours to days
+  timeDiff = Math.floor(timeDiff / 24);
+
+  // the rest of timeDiff is number of days
+  let days = timeDiff; //add days to hours
+
+  let totalHours = hours + days * 24;
+  totalHours = totalHours < 10 ? "0" + totalHours : totalHours;
+
+  if (totalHours === "00") {
+    return minutes + ":" + seconds;
+  } else {
+    return totalHours + ":" + minutes + ":" + seconds;
+  }
 }
